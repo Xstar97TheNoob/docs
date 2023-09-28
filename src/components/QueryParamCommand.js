470182cac9a4f36cffda67aba94578e350b348f1
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-27 21:44:25 -0400

    update

--- src/components/QueryParamCommand.js
+++ src/components/QueryParamCommand.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
-function QueryParamCommand({ text, queryParams = {}, queries }) {
+function QueryParamCommand({ text, defaults = {}, allowBlankIp = false, queries }) {
   const queryValues = queries.map((query) => {
-    const param = queryParams[query] || '';
-    return param ? `${query} ${param}` : '';
+    const param = new URLSearchParams(window.location.search).get(query);
+    if (param !== null && param !== undefined) {
+      return `${query} ${param}`;
+    } else if (query === 'ip' && allowBlankIp) {
+      return ''; // Leave IP blank if allowed
+    } else if (defaults[query] !== undefined) {
+      return `${query} ${defaults[query]}`;
+    } else {
+      return '';
+    }
   });
 
-  const command = [text, ...queryValues].join(' ');
+  const command = [text, ...queryValues].filter((value) => value !== '').join(' ');
 
   return (
     <div>
-      <code>{command}</code>
+      <pre>
+        <code className="language-shell">{command}</code>
+      </pre>
     </div>
   );
 }
