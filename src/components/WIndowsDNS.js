41f28151003502eca21e27f8553a68a37faa799d
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-27 20:39:07 -0400

    update

---
+++ src/components/WIndowsDNS.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+
+function WindowsDNS() {
+  useEffect(() => {
+    // Get query parameters from the URL
+    const params = new URLSearchParams(window.location.search);
+    const domain = params.get('domain') || 'app.mydomain.tld';
+    const ip = params.get('ip') || '10.0.0.123';
+
+    // Use the 'domain' and 'ip' values to update DNS settings
+    // You can perform any necessary actions here, e.g., making API requests
+
+    console.log(`Custom DNS Configuration - Domain: ${domain}, IP: ${ip}`);
+  }, []);
+
+  return (
+    <div>
+      <h1>Custom DNS Configuration</h1>
+      Domain: ${domain}
+      IP: ${ip}
+    </div>
+  );
+}
+
+export default WindowsDNS;
