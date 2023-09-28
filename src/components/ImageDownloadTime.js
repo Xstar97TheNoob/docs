02d6e8c94d6753fa7afc5b40d7ed3d8eb9541fb2
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-27 23:13:31 -0400

    update

--- src/components/ImageDownloadTime.js
+++ src/components/ImageDownloadTime.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-const DownloadTime = ({ size = 450, speed = 50 }) => {
+const DownloadTime = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  // Get the size and speed query parameters from the URL, or use default values if they are not provided
+  const size = queryParams.get("size") ? Number(queryParams.get("size")) : 450;
+  const speed = queryParams.get("speed") ? Number(queryParams.get("speed")) : 50;
+
   const [inputSize, setInputSize] = useState(size);
   const [inputSpeed, setInputSpeed] = useState(speed);
   const [time, setTime] = useState(0);
@@ -19,49 +27,8 @@ const DownloadTime = ({ size = 450, speed = 50 }) => {
   const color = time <= 120 ? 'green' : 'red';
 
   const styles = {
-    container: {
-      backgroundColor: 'var(--ifm-background-color)',
-      color: 'var(--ifm-font-color-base)',
-      boxShadow: 'var(--ifm-box-shadow)',
-      borderRadius: 'var(--ifm-border-radius)',
-      padding: '20px',
-      border: '1px solid var(--ifm-border-color)',
-      maxWidth: '400px',
-      margin: '0 auto',
-      textAlign: 'center', // Center align the child elements
-    },
-    title: {
-      fontSize: '1.5rem',
-      fontWeight: 'bold',
-      marginBottom: '10px',
-    },
-    inputContainer: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      marginBottom: '10px',
-    },
-    inputLabel: {
-      marginBottom: '5px',
-    },
-    inputField: {
-      display: 'flex',
-      alignItems: 'center',
-    },
-    inputUnit: {
-      marginLeft: '5px',
-    },
-    button: {
-      marginTop: '10px',
-    },
-    result: {
-      color,
-      marginTop: '10px',
-      fontSize: '1.2rem',
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  };  
+    // ... (your existing styles)
+  };
 
   return (
     <div style={styles.container}>
