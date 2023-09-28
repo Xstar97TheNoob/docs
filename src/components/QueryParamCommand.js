6f1f0402f895198448f1b540d655e0b088f12943
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-27 22:43:55 -0400

    update

--- src/components/QueryParamCommand.js
+++ src/components/QueryParamCommand.js
@@ -1,58 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 
-function QueryParamCommand({ text, domain: defaultDomain = 'app.example.com', ip: defaultIp = '', queries }) {
-  const [queryValues, setQueryValues] = useState([]);
-  const [domain, setDomain] = useState(defaultDomain);
-  const [ip, setIp] = useState(defaultIp);
+function QueryParamCommand({ text, defaults = {}, queriesToReplace = [], queries }) {
+  const queryParams = new URLSearchParams(window.location.search);
 
-  useEffect(() => {
-    // Function to extract query parameter values
-    const getQueryParamValues = () => {
-      const updatedDomain = new URLSearchParams(window.location.search).get('domain');
-      const updatedIp = new URLSearchParams(window.location.search).get('ip');
-      if (updatedDomain !== null) {
-        setDomain(updatedDomain);
-      }
-      if (updatedIp !== null) {
-        setIp(updatedIp);
-      }
+  // Replace specified query parameters with their default values
+  queriesToReplace.forEach((query) => {
+    if (queryParams.has(query)) {
+      queryParams.set(query, defaults[query] || '');
+    }
+  });
 
-      return queries.map((query) => {
-        const param = new URLSearchParams(window.location.search).get(query) || '';
-        return param ? `${query} ${param}` : '';
-      });
-    };
+  const queryValues = queries.map((query) => {
+    const param = queryParams.get(query);
+    if (param !== null && param !== undefined) {
+      return `${query} ${param}`;
+    } else if (defaults[query] !== undefined) {
+      return `${query} ${defaults[query]}`;
+    } else {
+      return '';
+    }
+  });
 
-    // Initial update of queryValues
-    setQueryValues(getQueryParamValues());
-
-    // Update queryValues whenever the query parameters change
-    const handleQueryParamChange = () => {
-      setQueryValues(getQueryParamValues());
-    };
-
-    window.addEventListener("popstate", handleQueryParamChange);
-
-    return () => {
-      window.removeEventListener("popstate", handleQueryParamChange);
-    };
-  }, [queries]);
-
-  // Remove empty values from queryValues
-  const filteredQueryValues = queryValues.filter((value) => value !== '');
-
-  // Create an array of values to include in the command
-  const valuesToInclude = [text];
-  if (domain !== defaultDomain) {
-    valuesToInclude.push(domain);
-  }
-  if (ip !== defaultIp) {
-    valuesToInclude.push(ip);
-  }
-  valuesToInclude.push(...filteredQueryValues);
-
-  // Join the values with a space to form the command
-  const command = valuesToInclude.join(' ');
+  const command = [text, ...queryValues].filter((value) => value !== '').join(' ');
 
   return (
     <div>
