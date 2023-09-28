532b09bf5eb66a22539e54b385eda5cf154f9a83
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-27 21:53:20 -0400

    update

--- src/components/QueryParamCommand.js
+++ src/components/QueryParamCommand.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
 
-
 function QueryParamCommand({ text, domain = 'app.example.com', ip = '', queries }) {
-  const queryValues = queries.map((query) => {
-    const param = new URLSearchParams(window.location.search).get(query) || '';
-    return param ? `${query} ${param}` : '';
-  });
+  const [queryValues, setQueryValues] = useState([]);
+
+  useEffect(() => {
+    // Check if window object is available (running in the browser)
+    if (typeof window !== "undefined") {
+      const queryValues = queries.map((query) => {
+        const param = new URLSearchParams(window.location.search).get(query) || '';
+        return param ? `${query} ${param}` : '';
+      });
+
+      setQueryValues(queryValues);
+    }
+  }, [queries]);
 
   const command = [text, domain, ip, ...queryValues].join(' ');
 
