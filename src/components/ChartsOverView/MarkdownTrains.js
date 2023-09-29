2f9ac601e7d4be423c37b3345bf5db22e760d4fb
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-29 04:31:53 -0400

    update

--- src/components/ChartsOverView/MarkdownTrains.js
+++ src/components/ChartsOverView/MarkdownTrains.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import HelperUtil, { capitalizeWords } from './HelperUtil.js';
 
 const MarkdownTrains = ({ trains }) => {
   return (
     <>
       {trains.map(train => (
         <>
-        <a href={`#${train.name}`}>{train.name}</a>
+        <a href={`#${capitalizeWords(train.name)}`}>{capitalizeWords(train.name)}</a>
         <br />
         </>
       ))}
