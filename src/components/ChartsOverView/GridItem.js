3a644617ed6f58da5a9455bc12934b37c3beb09d
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-09-29 03:29:59 -0400

    update

--- src/components/ChartsOverView/GridItem.js
+++ src/components/ChartsOverView/GridItem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import '../grid.module.css';
+import './grid.module.css';
 import HelperUtil, { isLink,getSourceName }  from './HelperUtil.js';
 
 const GridItem = ({chart,onClick}) => {
