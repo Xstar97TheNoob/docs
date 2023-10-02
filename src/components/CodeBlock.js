bb2778fbb08001b86b69a8cece047b42b562ab85
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-10-02 19:17:38 -0400

    update

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,28 +1,28 @@
 import React, { useState } from "react";
 
 const CodeBlock = ({ code, queryParams }) => {
-  const generateQueryParams = () => {
-    if (!queryParams || !Array.isArray(queryParams) || queryParams.length === 0) {
-      return '';
-    }
-
-    const queryParamsStr = queryParams
-      .map((param) => {
-        if (typeof param === 'string') {
-          return param;
-        } else if (typeof param === 'object' && param.defaultValue) {
-          return param.defaultValue;
-        }
+    const generateQueryParams = () => {
+      if (!queryParams || !Array.isArray(queryParams) || queryParams.length === 0) {
         return '';
-      })
-      .join(' ');
-
-    return queryParamsStr ? `${queryParamsStr}` : '';
+      }
+  
+      const queryParamsStr = queryParams
+        .map((param) => {
+          if (typeof param === 'string') {
+            return param;
+          } else if (typeof param === 'object' && param.name && param.value) {
+            return param.value;
+          }
+          return '';
+        })
+        .join(' ');
+  
+      return queryParamsStr ? `${queryParamsStr}` : '';
+    };
+  
+    return (
+      <>{`\`\`\`shell\n\n${code} ${generateQueryParams()}\n\n\`\`\``}</>
+    );
   };
-
-  return (
-    <>{`\`\`\`shell\n${code} ${generateQueryParams()}\n\`\`\``}</>
-  );
-};
-
-export default CodeBlock;
+  
+  export default CodeBlock;
