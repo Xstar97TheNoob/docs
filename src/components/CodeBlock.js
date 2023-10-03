1d7013bd89ba8cd3045f94e843979a80242e9ad7
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-10-02 20:25:22 -0400

    update

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import { useLocation } from "react-router-dom";
 
 const CodeBlock = ({ code, queryParams }) => {
@@ -13,25 +12,16 @@ const CodeBlock = ({ code, queryParams }) => {
         queryParamObject[key] = value;
       }
   
-      // If there are no query parameters in the URL, use the default values from queryParams prop
-      if (Object.keys(queryParamObject).length === 0) {
-        const replacedCode = code.replace(/{(\w+)}/g, (match, paramName) => {
-          const queryParam = queryParams.find(param => param.name === paramName);
-          return queryParam ? queryParam.value : match;
-        });
-        return replacedCode;
-      }
-  
       // Merge query parameters from the URL with the provided queryParams prop
-      const mergedQueryParams = { ...queryParams };
+      const mergedQueryParams = { ...queryParams, ...queryParamObject };
   
       // Replace placeholders in the code with the actual query parameter values
       const replacedCode = code.replace(/{(\w+)}/g, (match, paramName) => {
         // Find the corresponding query parameter
         const queryParamValue = mergedQueryParams[paramName];
   
-        // If the query parameter exists and has a value, use it; otherwise, keep the placeholder
-        return queryParamValue !== undefined ? queryParamValue : match;
+        // If the query parameter exists and has a value, use it; otherwise, use the default value
+        return queryParamValue !== undefined ? queryParamValue : queryParams[paramName]?.value || match;
       });
   
       return replacedCode;
