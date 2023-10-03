cf5c253000c268388be238786a02f6a48b59a0ab
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-10-02 20:03:33 -0400

    update

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,36 +1,35 @@
 import React, { useState } from "react";
 
 const CodeBlock = ({ code, queryParams }) => {
-  const generateQueryParams = () => {
-    if (!queryParams || !Array.isArray(queryParams) || queryParams.length === 0) {
-      return code;
-    }
-
-    // Parse the URL query string into an object
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const queryParamObject = {};
-
-    for (const [key, value] of urlSearchParams.entries()) {
-      queryParamObject[key] = value;
-    }
-
-    // Replace placeholders in the code with the actual query parameter values
-    const replacedCode = code.replace(/{(\w+)}/g, (match, paramName) => {
-      // Find the corresponding query parameter
-      const queryParamValue = queryParamObject[paramName];
-
-      // If the query parameter exists and has a value, use it; otherwise, keep the placeholder
-      return queryParamValue !== undefined ? queryParamValue : match;
-    });
-
-    return replacedCode;
+    const generateQueryParams = () => {
+      // Parse the URL query string into an object
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const queryParamObject = {};
+  
+      for (const [key, value] of urlSearchParams.entries()) {
+        queryParamObject[key] = value;
+      }
+  
+      // Merge query parameters from the URL with the provided queryParams prop
+      const mergedQueryParams = { ...queryParamObject, ...queryParams };
+  
+      // Replace placeholders in the code with the actual query parameter values
+      const replacedCode = code.replace(/{(\w+)}/g, (match, paramName) => {
+        // Find the corresponding query parameter
+        const queryParamValue = mergedQueryParams[paramName];
+  
+        // If the query parameter exists and has a value, use it; otherwise, keep the placeholder
+        return queryParamValue !== undefined ? queryParamValue : match;
+      });
+  
+      return replacedCode;
+    };
+  
+    return (
+      <pre>
+        <code>{generateQueryParams()}</code>
+      </pre>
+    );
   };
-
-  return (
-    <pre>
-      <code>{generateQueryParams()}</code>
-    </pre>
-  );
-};
-
-export default CodeBlock;
+  
+  export default CodeBlock;
