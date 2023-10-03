83ed351619f9abee38c0576e8ccfb05ce0f50ee9
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-10-03 00:10:22 -0400

    update

--- src/components/ChartsOverView/LoadingView.js
+++ src/components/ChartsOverView/LoadingView.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const LoadingView = ({src,msg}) => (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <img src={src} alt="loading" />
+      <Svg src={src} alt="loading" />
       <p style={{ marginTop: '10px' }}>{msg}</p>
     </div>
   );
