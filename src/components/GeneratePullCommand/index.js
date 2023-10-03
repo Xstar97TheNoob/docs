6e03bdd030630935cb2040249b0b52a92baefb01
Xstar97TheNoob <9399967+xstar97@users.noreply.github.com>
2023-10-02 22:18:54 -0400

    update

--- src/components/GeneratePullCommand/index.js
+++ src/components/GeneratePullCommand/index.js
@@ -2,33 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 const GeneratePullCommand = () => {
   const [inputText, setInputText] = useState('');
-  const [loading, setLoading] = useState(false);
   const [outputCommands, setOutputCommands] = useState([]);
 
   useEffect(() => {
-    if (inputText) {
-      // Text has been added to the "App Events" textarea, auto-generate commands.
-      setLoading(true);
-      // Simulate processing the input text to generate output commands
-      setTimeout(() => {
-        const lines = inputText.split('\n');
-        const output = [];
+    const lines = inputText.split('\n');
+    const output = [];
 
-        lines.forEach((line) => {
-          if (line.includes('Back-off pulling image')) {
-            const imageNameMatch = line.match(/"([^"]+)"/);
-            if (imageNameMatch) {
-              const imageName = imageNameMatch[1];
-              const command = `sudo docker pull ${imageName}`;
-              output.push(command);
-            }
-          }
-        });
+    lines.forEach((line) => {
+      if (line.includes('Back-off pulling image')) {
+        const imageNameMatch = line.match(/"([^"]+)"/);
+        if (imageNameMatch) {
+          const imageName = imageNameMatch[1];
+          const command = `sudo docker pull ${imageName}`;
+          output.push(command);
+        }
+      }
+    });
 
-        setOutputCommands(output);
-        setLoading(false);
-      }, 2000);
-    }
+    setOutputCommands(output);
   }, [inputText]);
 
   return (
@@ -45,14 +36,8 @@ const GeneratePullCommand = () => {
             placeholder="Enter App Events here..."
             value={inputText}
             onChange={(e) => setInputText(e.target.value)}
-            disabled={loading}
           />
         </div>
-        {loading && (
-          <div className="loading-spinner">
-            <div className="spinner"></div>
-          </div>
-        )}
         {outputCommands.length > 0 && (
           <div className="output-commands">
             <h3>Generated Commands:</h3>
