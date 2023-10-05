6e51070d52c2c4f43359a01386b18fc48a875ef9
xstar97 <dev.xstar97@gmail.com>
2023-10-05 19:03:48 -0400

    update

--- src/components/AppsTablev2/index.js
+++ src/components/AppsTablev2/index.js
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from "react";
+import loadingViewSrc from '/img/loading-aesthetic.gif';
+import LoadingView from './LoadingView.js';
 import jsonData from '/servers/servers.json';
 
 const AppsTablev2 = () => {
+  const loadingViewMsg = "Loading server data...";
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     if (jsonData && jsonData.length > 0) {
-      setData(jsonData); // Use the entire JSON array if it's not empty or undefined
+      // Simulate loading delay for demonstration purposes
+      setTimeout(() => {
+        setData(jsonData);
+        setIsLoading(false); // Set isLoading to false when data is ready
+      }, 2000); // Adjust the delay time as needed
     }
   }, []);
+
+  if (isLoading) {
+    // Render a loading view while data is being fetched
+    return <LoadingView src={loadingViewSrc} msg={loadingViewMsg} />
+  }
 
   if (!data || data.length === 0) {
     // Handle the case when jsonData is empty or undefined
@@ -19,7 +32,7 @@ const AppsTablev2 = () => {
     <div>
       {data.map((server, index) => (
         <div key={index}>
-          < br />
+          <br />
           <a href={`#${server.serverName}`} id={server.serverName}><h2>{server.serverName}</h2></a><hr />
           <p>{server.serverDescription}</p>
           <table>
