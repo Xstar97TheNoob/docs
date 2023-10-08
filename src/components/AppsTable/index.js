29e60279070f147b7512ab631b44c85463698cc5
xstar97 <dev.xstar97@gmail.com>
2023-10-08 03:30:53 -0400

    update

--- src/components/AppsTable/index.js
+++ src/components/AppsTable/index.js
@@ -5,60 +5,78 @@ const AppsTable = () => {
   const loadingViewMsg = "Loading server data...";
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [dataLoaded, setDataLoaded] = useState(false); // Track if data has been loaded
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [openTables, setOpenTables] = useState({}); // State to track open tables
+
+  function replaceCharWithChar(inputString, oldChar, charToReplaceWith) {
+    const regex = new RegExp(oldChar, 'g');
+    const modifiedString = inputString.replace(regex, charToReplaceWith);
+    return modifiedString;
+  }
 
   useEffect(() => {
-    if (!dataLoaded) { // Check if data has not been loaded
+    if (!dataLoaded) {
       if (jsonData && jsonData.length > 0) {
         setData(jsonData);
         setIsLoading(false);
-        setDataLoaded(true); // Set dataLoaded to true once data is loaded
+        setDataLoaded(true);
       }
     }
-  }, [dataLoaded]); // Trigger this effect only when dataLoaded changes
+  }, [dataLoaded]);
 
   if (isLoading) {
-    // Render a loading view while data is being fetched
     return <p>{loadingViewMsg}</p>;
   }
 
   if (!data || data.length === 0) {
-    // Handle the case when jsonData is empty or undefined
     return <p>No server data available. 😔</p>;
   }
+
+  const toggleTable = (serverName) => {
+    // Toggle the open state of the table for a given server
+    setOpenTables(prevOpenTables => ({
+      ...prevOpenTables,
+      [serverName]: !prevOpenTables[serverName],
+    }));
+  };
 
   return (
     <div>
       {data.map((server, index) => (
         <div key={index}>
           <br />
-          <a href={`#${server.serverName.replace(" ","-")}`} id={server.serverName.replace(" ","-")}><h2>{server.serverName}</h2></a><hr />
-          <p>{server.serverDescription}</p>
-          <table>
-            <thead>
-              <tr>
-                <th>Icon</th>
-                <th>Name</th>
-                <th>Train</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              {server.apps &&
-                server.apps.map(app => (
-                  <tr key={app.name}>
-                    <td><img src={app.icon} alt={app.name} width="48" height="36" /></td>
-                    <td>
-                      {app.name}
-                      {app.instances > 1 && <sup>x{app.instances}</sup>}
-                    </td>
-                    <td>{app.train}</td>
-                    <td>{app.description}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-          <p>Currently Installed: {server.count}</p>
+          <a href={`#${replaceCharWithChar(server.serverName, " ", "-")}`} id={replaceCharWithChar(server.serverName, " ", "-")}>
+            <h2 onClick={() => toggleTable(server.serverName)}>{server.serverName}</h2>
+          </a>
+          <hr />
+          <div className={`table-container ${openTables[server.serverName] ? 'open' : 'closed'}`}>
+            <p>{server.serverDescription}</p>
+            <table>
+              <thead>
+                <tr>
+                  <th>Icon</th>
+                  <th>Name</th>
+                  <th>Train</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                {server.apps &&
+                  server.apps.map(app => (
+                    <tr key={app.name}>
+                      <td><img src={app.icon} alt={app.name} width="48" height="36" /></td>
+                      <td>
+                        {app.name}
+                        {app.instances > 1 && <sup>x{app.instances}</sup>}
+                      </td>
+                      <td>{app.train}</td>
+                      <td>{app.description}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+            <p>Currently Installed: {server.count}</p>
+          </div>
           <hr />
         </div>
       ))}
